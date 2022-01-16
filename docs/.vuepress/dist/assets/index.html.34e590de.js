ccf948334dffecf52253aff027bc2a95587b779f
aibayanyu <aibayanyu@qq.com>
2022-01-16 19:14:57 +0800

    fix: 修改项目信息

---
+++ docs/.vuepress/dist/assets/index.html.34e590de.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1642327599e3,contributors:[{name:"zhuzhengjian",email:"aibyanyu@qq.com",commits:1}]},filePathRelative:"index.md"};export{e as data};
---
+++ docs/info.md
@@ -0,0 +1,10 @@
+# 测试
+
+{{ data }}
+
+<script setup lang="ts">
+    import { ref } from "vue";
+    const data = ref(false);
+</script>
+
+<demo src="./example/test.vue" title="案例" desc="这是一个代码块的基础案例"></demo>
