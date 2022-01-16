fb66cbb05154683693651e512ac8f429ceaea2bb
zhuzhengjian <aibyanyu@qq.com>
2022-01-16 18:06:39 +0800

    fix: init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+import { DefaultThemeOptions, ViteBundlerOptions } from "vuepress";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { codeBlockPlugin } from "@yanyu-fe/vuepress-plugin-code-block";
+export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
+    title: "文档中心",
+    bundlerConfig: {
+        viteOptions: {
+            server: {
+                fs: {
+                    strict: false,
+                },
+            },
+            css: {
+                preprocessorOptions: {
+                    less: {
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+            plugins: [vueJsx()],
+        },
+    },
+    plugins: [[codeBlockPlugin, {}]],
+});
---
+++ docs/example/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{counter}}
+    <button @click="add">增加</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue"
+const counter = ref(0);
+const add = () => {
+  counter.value ++ ;
+}
+</script>
+
