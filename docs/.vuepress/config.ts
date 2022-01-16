df56fe380cad4b51c63fb68729595f5ea9f55592
aibayanyu <aibayanyu@qq.com>
2022-01-16 19:28:41 +0800

    fix: 支持国际化

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,20 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { codeBlockPlugin } from "@yanyu-fe/vuepress-plugin-code-block";
 export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
     title: "文档中心",
+    locales:{
+        '/':{
+            lang:"zh-CN",
+            title:"QA文档中心"
+        }
+    },
+    themeConfig:{
+        locales:{
+            '/':{
+                lastUpdatedText:"最后更新",
+                contributorsText:"贡献者"
+            }
+        }
+    },
     bundlerConfig: {
         viteOptions: {
             server: {
